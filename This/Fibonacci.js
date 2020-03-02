d8e42529b00fd8f35179e0f168b8ee66b10d5e39
Vlad <vladimirmolav@abv.bg>
2020-03-02 12:15:58 +0200

    xd

---
+++ This/Fibonacci.js
@@ -0,0 +1,13 @@
+
+function getFibonator() {
+    let previous = 0;
+    let current = 1;
+
+    return function () {
+        let result = previous + current;
+        previous = current;
+        current = result;
+
+        return previous;
+    }
+}
